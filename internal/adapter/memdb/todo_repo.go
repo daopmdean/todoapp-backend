316e4c9c5ead55cfa319d01c22ecdbbd5c4dba30
Dao Pham <daopm.dean@gmail.com>
2021-09-07 21:32:22 +0700

    feat: todo repo memdb implementation

--- internal/adapter/memdb/todo_repo.go
+++ internal/adapter/memdb/todo_repo.go
@@ -1,7 +1,34 @@
 package memdb
 
-import "todoapp/internal/usecase/repo"
+import (
+	"todoapp/internal/entity"
+	"todoapp/internal/usecase/repo"
+)
 
-func NewTodoRepo() repo.TodoRepo {
+func NewTodoRepo(ur repo.UserRepo) repo.TodoRepo {
+	return &todoRepo{
+		userRepo: ur,
+	}
+}
+
+type todoRepo struct {
+	userRepo repo.UserRepo
+}
+
+func (tr *todoRepo) GetTodoList(username string) []entity.TodoItem {
+
+	return nil
+}
+
+func (tr *todoRepo) CreateTodoItem(tds []entity.TodoItem, td *entity.TodoItem) error {
+	tds = append(tds, *td)
+	return nil
+}
+
+func (tr *todoRepo) CheckTodoItem(tds []entity.TodoItem, td *entity.TodoItem) error {
+	return nil
+}
+
+func (tr *todoRepo) DeleteTodoItem(tds []entity.TodoItem, td *entity.TodoItem) error {
 	return nil
 }
