dbaaa75b7000468cdb6d0d47ab0000926727280d
Dao Pham <daopm.dean@gmail.com>
2021-09-14 19:37:19 +0700

    feat: choose db mode

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"os"
 	"todoapp/internal/adapter/http/rest"
+	"todoapp/internal/adapter/memdb"
 	"todoapp/internal/adapter/mssqldb"
 	"todoapp/internal/common/config"
 	"todoapp/internal/entity"
+	"todoapp/internal/usecase/repo"
 
 	"gorm.io/gorm"
 )
 
 func main() {
-	db, err := mssqldb.Connect()
-	if err != nil {
-		panic("can't connect to database")
-	}
-	migrateDB(db)
+	var userRepo repo.UserRepo
+	var todoRepo repo.TodoRepo
 
-	userRepo := mssqldb.NewUserRepo(db)
-	todoRepo := mssqldb.NewTodoRepo(db)
+	switch os.Getenv("DB_MODE") {
+	case "mssql":
+		db, err := mssqldb.Connect()
+		if err != nil {
+			panic("can't connect to database")
+		}
+		migrateDB(db)
+
+		userRepo = mssqldb.NewUserRepo(db)
+		todoRepo = mssqldb.NewTodoRepo(db)
+	default:
+		userRepo = memdb.NewUserRepo()
+		todoRepo = memdb.NewTodoRepo()
+	}
 
 	s := rest.NewServer(userRepo, todoRepo)
 	p := config.GetPort()
